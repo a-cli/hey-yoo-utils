e5e9065835f8b547cd5df4b47dd9fbdba3d7e1e0
Haolinhom <haolinhom@gmail.com>
2020-08-22 21:35:27 +0800

    feat: add batchRegCommand util;

---
+++ src/batchRegCommand.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Object} program (Command object's instance. Create by commander package)
+ * @param {Array} list (command config list)
+ * */
+function batchRegCommand(program, list) {
+	list.forEach((cfg) => {
+		let moduleSource = cfg.source || null;
+		delete cfg.source;
+		const keys = Object.keys(cfg);
+		let tempOne = program;
+		keys.forEach((key) => {
+			tempOne = tempOne[key](cfg[key]);
+		});
+		if (!cfg.action && moduleSource) {
+			let tempModule = require(moduleSource);
+			tempOne.action(tempModule);
+		}
+	});
+}
+
+module.exports = batchRegCommand;
