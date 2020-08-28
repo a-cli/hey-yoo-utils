e66821c91349e0daf22b761fabce6238ed98970b
HaolinHom <haolinhom@gmail.com>
2020-08-28 15:13:48 +0800

    fix: parseArgs; update version;

--- src/parseArgs.js
+++ src/parseArgs.js
@@ -1,3 +1,10 @@
+const needParseCfg = {
+  'true': true,
+  'false': false,
+  'undefined': undefined,
+  'null': null,
+};
+
 /**
  * @param {Array} args
  * @return {Object} result
@@ -5,11 +12,14 @@
 function parseArgs(args) {
   if (Array.isArray(args)) {
     let result = {};
-    for (let i = 1; i < args.length; i++) {
+    for (let i = 0; i < args.length; i++) {
       if (/^(?!=).*=.*/.test(args[i])) {
         let temp = args[i].split('=');
         let key = temp.shift();
         result[key] = temp.join('=');
+        if (needParseCfg.hasOwnProperty(result[key])) {
+          result[key] = needParseCfg[result[key]];
+        }
       } else {
         result[args[i]] = true;
       }
