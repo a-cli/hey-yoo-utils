b82e23a4afaf7689ff1731b6f3cd05c07816b460
HaolinHom <haolinhom@gmail.com>
2020-09-10 14:26:58 +0800

    feat: chalk.color;

--- src/chalk.js
+++ src/chalk.js
@@ -1,3 +1,26 @@
 const chalk = require('chalk');
 
+// Material Design color 400
+chalk.color = {
+  red: chalk.hex('#EF5350'),
+  pink: chalk.hex('#EC407A'),
+  purple: chalk.hex('#AB47BC'),
+  deepPurple: chalk.hex('#7E57C2'),
+  indigo: chalk.hex('#5C6BC0'),
+  blue: chalk.hex('#42A5F5'),
+  lightBlue: chalk.hex('#29B6F6'),
+  cyan: chalk.hex('#26C6DA'),
+  teal: chalk.hex('#26A69A'),
+  green: chalk.hex('#66BB6A'),
+  lightGreen: chalk.hex('#9CCC65'),
+  lime: chalk.hex('#D4E157'),
+  yellow: chalk.hex('#FFEE58'),
+  amber: chalk.hex('#FFCA28'),
+  orange: chalk.hex('#FFA726'),
+  deepOrange: chalk.hex('#FF7043'),
+  brown: chalk.hex('#8D6E63'),
+  gray: chalk.hex('#BDBDBD'),
+  blueGray: chalk.hex('#78909C'),
+};
+
 module.exports = chalk;
