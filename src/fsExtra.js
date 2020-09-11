e43d19d25178eec66010846908c57f66a72768f8
HaolinHom <haolinhom@gmail.com>
2020-09-11 16:21:25 +0800

    feat: add fsExtra.remove and update version to 0.2.1;

--- src/fsExtra.js
+++ src/fsExtra.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 function ensureDir(path) {
 	if (fs.existsSync(path)) {
@@ -10,6 +11,25 @@ function ensureDir(path) {
 	fs.mkdirSync(path);
 }
 
+function remove(tagPath) {
+	if (fs.existsSync(tagPath)) {
+		const stats = fs.statSync(tagPath);
+		if (stats.isFile(tagPath)) {
+			fs.unlinkSync(tagPath);
+		} else if (stats.isDirectory()) {
+			const fileList = fs.readdirSync(tagPath);
+			if (fileList.length > 0) {
+				fileList.forEach((file) => {
+					const filePath = path.join(tagPath, file);
+					remove(filePath);
+				});
+			}
+			fs.rmdirSync(tagPath);
+		}
+	}
+}
+
 module.exports = {
 	ensureDir,
+	remove,
 };
